6e3731ac490d23c36cdd9c21cddbdeb586964ffe
Shockynio <74144399+Shockynio@users.noreply.github.com>
2023-04-20 19:16:31 +0200

    start

---
+++ app/src/main/java/com/example/trabajo_final_grupo_11_dam/MainActivity.java
@@ -0,0 +1,14 @@
+package com.example.trabajo_final_grupo_11_dam;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
