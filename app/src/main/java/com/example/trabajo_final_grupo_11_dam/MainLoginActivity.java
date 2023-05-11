8f69967bbe176b41b3e515beb933c4a7deab12af
EnriqueRapo <126591159+EnriqueRapo@users.noreply.github.com>
2023-05-11 13:14:38 +0200

    Menu en menu Restaurante "Aun por terminar"

--- app/src/main/java/com/example/trabajo_final_grupo_11_dam/MainLoginActivity.java
+++ app/src/main/java/com/example/trabajo_final_grupo_11_dam/MainLoginActivity.java
@@ -8,6 +8,8 @@ import android.content.res.Resources;
 import android.graphics.drawable.Drawable;
 import android.graphics.drawable.LayerDrawable;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -128,6 +130,7 @@ public class MainLoginActivity extends AppCompatActivity {
         AlertDialog dialog = builder.create();
         dialog.show();
     }
+
 }
 
 
--- app/src/main/java/com/example/trabajo_final_grupo_11_dam/MenuRestaurantesActivity.java
+++ app/src/main/java/com/example/trabajo_final_grupo_11_dam/MenuRestaurantesActivity.java
@@ -1,6 +1,9 @@
 package com.example.trabajo_final_grupo_11_dam;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,4 +13,27 @@ public class MenuRestaurantesActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_restaurantes);
     }
+/*
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.overflow, menu);
+        return true;
+    }
+
+    public boolean onOptionsItemSelected(MenuItem item) {
+
+        int id = item.getItemId();
+
+        if (id == R.id.btn_menurestaurante_carta) {
+            Toast.makeText(this, "boton1", Toast.LENGTH_LONG).show();
+
+        }else if (id==R.id.btn_menurestaurante_añadirCarta){
+            Toast.makeText(this, "boton2", Toast.LENGTH_LONG).show();
+        }else  if (id==R.id.btn_menurestaurante_pedidos){
+            Toast.makeText(this, "boton3", Toast.LENGTH_LONG).show();
+        }else if (id==R.id.btn_menurestaurante_cerrarsesion){
+            Toast.makeText(this, "boton4", Toast.LENGTH_LONG).show();
+        }
+
+    return  super.onOptionsItemSelected(item);
+    }*/
 }
