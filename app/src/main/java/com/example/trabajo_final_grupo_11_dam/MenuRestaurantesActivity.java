67a22358098e5472dca56d5dbca116e43a2a532f
EnriqueRapo <126591159+EnriqueRapo@users.noreply.github.com>
2023-05-12 16:39:32 +0200

    Layout "Añadir carta", "Aun por terminar fallo en scroll"

--- app/src/main/java/com/example/trabajo_final_grupo_11_dam/MenuRestaurantesActivity.java
+++ app/src/main/java/com/example/trabajo_final_grupo_11_dam/MenuRestaurantesActivity.java
@@ -2,19 +2,53 @@ package com.example.trabajo_final_grupo_11_dam;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 import android.widget.Toast;
 
 public class MenuRestaurantesActivity extends AppCompatActivity {
+
+    private Button   btnverCarta;
+    private Button   btnañadirCarta;
+    private Button   btnpedidos;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu_restaurantes);
-    }
 
+        btnañadirCarta =(Button) findViewById(R.id.btn_añadir_carta);
+        btnverCarta = (Button)findViewById(R.id.btn_ver_carta);
+        btnpedidos = (Button)findViewById(R.id.btn_pedidos);
+
+        btnañadirCarta.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                switch (view.getId()) {
+
+                    case R.id.btn_añadir_carta:
+
+                        Intent i = new Intent(MenuRestaurantesActivity.this, AnyadirCartaActivity.class);
+                        startActivity(i);
+                        finish();
+                        break;
+
+                    case R.id.btn_ver_carta:
+
+                        findViewById(R.id.btn_enviar).setVisibility(View.VISIBLE);
+                        break;
+                    case R.id.btn_pedidos:
+                        break;
+                }
+            }
+
+        });
+/*
     public boolean onCreateOptionsMenu(Menu menu) {
         getMenuInflater().inflate(R.menu.overflow, menu);
         return true;
@@ -36,5 +70,7 @@ public class MenuRestaurantesActivity extends AppCompatActivity {
         }
 
         return  super.onOptionsItemSelected(item);
-    }
+    }*/
+}
+
 }
