025ecf0843a48441cc2a9cb07dd4b7d0a66a83e8
EnriqueRapo <126591159+EnriqueRapo@users.noreply.github.com>
2023-05-15 19:53:27 +0200

    Menu

---
+++ app/src/main/java/com/example/trabajo_final_grupo_11_dam/ui/home/HomeViewModel.java
@@ -0,0 +1,19 @@
+package com.example.trabajo_final_grupo_11_dam.ui.home;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class HomeViewModel extends ViewModel {
+
+    private final MutableLiveData<String> mText;
+
+    public HomeViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is home fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
