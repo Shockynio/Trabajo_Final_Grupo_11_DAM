90b9a30e2d3f5ca0a67944b925d15c3cb26555ad
EnriqueRapo <126591159+EnriqueRapo@users.noreply.github.com>
2023-05-17 23:14:09 +0200

    Funciones onCLick de  (fragment_anyadir_carta.xml, fragment_carta.xml, fragment_restaurante.xml, fragment_home.xml) Falta que se comunique con la base de datos

--- app/src/main/java/com/example/trabajo_final_grupo_11_dam/Fragment/EncargosEscogidosragment.java
+++ app/src/main/java/com/example/trabajo_final_grupo_11_dam/Fragment/EncargosEscogidosragment.java
@@ -1,4 +1,4 @@
-package com.example.trabajo_final_grupo_11_dam;
+package com.example.trabajo_final_grupo_11_dam.Fragment;
 
 import android.os.Bundle;
 
@@ -8,12 +8,14 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.example.trabajo_final_grupo_11_dam.R;
+
 /**
  * A simple {@link Fragment} subclass.
- * Use the {@link anyadirCartaFragment#newInstance} factory method to
+ * Use the {@link EncargosEscogidosragment#newInstance} factory method to
  * create an instance of this fragment.
  */
-public class anyadirCartaFragment extends Fragment {
+public class EncargosEscogidosragment extends Fragment {
 
     // TODO: Rename parameter arguments, choose names that match
     // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
@@ -24,7 +26,7 @@ public class anyadirCartaFragment extends Fragment {
     private String mParam1;
     private String mParam2;
 
-    public anyadirCartaFragment() {
+    public EncargosEscogidosragment() {
         // Required empty public constructor
     }
 
@@ -34,11 +36,11 @@ public class anyadirCartaFragment extends Fragment {
      *
      * @param param1 Parameter 1.
      * @param param2 Parameter 2.
-     * @return A new instance of fragment anyadirCartaFragment.
+     * @return A new instance of fragment EncargosEscogidosragment.
      */
     // TODO: Rename and change types and number of parameters
-    public static anyadirCartaFragment newInstance(String param1, String param2) {
-        anyadirCartaFragment fragment = new anyadirCartaFragment();
+    public static EncargosEscogidosragment newInstance(String param1, String param2) {
+        EncargosEscogidosragment fragment = new EncargosEscogidosragment();
         Bundle args = new Bundle();
         args.putString(ARG_PARAM1, param1);
         args.putString(ARG_PARAM2, param2);
@@ -59,6 +61,6 @@ public class anyadirCartaFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_anyadir_carta, container, false);
+        return inflater.inflate(R.layout.fragment_encargos_escogidosragment, container, false);
     }
 }
