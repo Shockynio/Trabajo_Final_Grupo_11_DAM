86254a7e3c7123db93f8c8531a050f9619480ab3
Shockynio <74144399+Shockynio@users.noreply.github.com>
2023-05-18 10:07:27 +0200

    menu barra icono --> luego lo acabo bien

--- app/src/main/java/com/example/trabajo_final_grupo_11_dam/MenuActivity.java
+++ app/src/main/java/com/example/trabajo_final_grupo_11_dam/MenuActivity.java
@@ -5,7 +5,13 @@ import android.view.MenuInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.Menu;
+import android.widget.TextView;
 
+import com.android.volley.Request;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
 import com.google.android.material.snackbar.Snackbar;
 import com.google.android.material.navigation.NavigationView;
 
@@ -18,10 +24,16 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import com.example.trabajo_final_grupo_11_dam.databinding.ActivityMenuBinding;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
 public class MenuActivity extends AppCompatActivity {
 
     private AppBarConfiguration mAppBarConfiguration;
     private ActivityMenuBinding binding;
+
+    private NavigationView navigationView;
+    private View headerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -33,19 +45,21 @@ public class MenuActivity extends AppCompatActivity {
         setSupportActionBar(binding.appBarMenu.toolbar);
 
         DrawerLayout drawer = binding.drawerLayout;
-        NavigationView navigationView = binding.navView;
+        navigationView = binding.navView;
         // Passing each menu ID as a set of Ids because each
         // menu should be considered as top level destinations.
         mAppBarConfiguration = new AppBarConfiguration.Builder(
-                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.perfilFragment,R.id.encargosDisponiblesFragment,R.id.encargosEscogidosragment,R.id.restauranteFragment,R.id.cartaFragment,
+                R.id.nav_home, R.id.nav_gallery, R.id.nav_slideshow, R.id.perfilFragment, R.id.encargosDisponiblesFragment, R.id.encargosEscogidosragment, R.id.restauranteFragment, R.id.cartaFragment,
                 R.id.anyadirCartaFragment)
                 .setOpenableLayout(drawer)
                 .build();
         NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_content_menu);
         NavigationUI.setupActionBarWithNavController(this, navController, mAppBarConfiguration);
         NavigationUI.setupWithNavController(navigationView, navController);
-    }
 
+        // Fetch user data and update the header view
+        fetchUserData();
+    }
 
     @Override
     public boolean onSupportNavigateUp() {
@@ -61,8 +75,6 @@ public class MenuActivity extends AppCompatActivity {
         return super.onCreateOptionsMenu(menu);
     }
 
-
-
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
         switch (item.getItemId()) {
@@ -75,6 +87,44 @@ public class MenuActivity extends AppCompatActivity {
         }
     }
 
+    private void fetchUserData() {
+        String url = "https://trabajo-final-grupo-11.azurewebsites.net/selectuserdata";
 
+        // Create a StringRequest with the GET method
+        StringRequest stringRequest = new StringRequest(Request.Method.GET, url,
+                new Response.Listener<String>() {
+                    @Override
+                    public void onResponse(String response) {
+                        // Parse the response and extract the user's information
+                        try {
+                            JSONObject jsonResponse = new JSONObject(response);
+                            String fullName = jsonResponse.getString("fullName");
+                            String email = jsonResponse.getString("email");
 
+                            // Update the header view with the retrieved user's information
+                            updateHeaderView(fullName, email);
+                        } catch (JSONException e) {
+                            e.printStackTrace();
+                        }
+                    }
+                },
+                new Response.ErrorListener() {
+                    @Override
+                    public void onErrorResponse(VolleyError error) {
+                        // Handle error response
+                    }
+                });
+
+        // Add the request to the request queue
+        Volley.newRequestQueue(this).add(stringRequest);
+    }
+
+    private void updateHeaderView(String fullName, String email) {
+        headerView = navigationView.getHeaderView(0);
+        TextView fullNameTextView = headerView.findViewById(R.id.tv_full_name);
+        TextView emailTextView = headerView.findViewById(R.id.tv_email);
+
+        fullNameTextView.setText(fullName);
+        emailTextView.setText(email);
+    }
 }
